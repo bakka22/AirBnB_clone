24554cacca7f73c6aa79647ea4679c947e5b7443
bakka22 <135711566+bakka22@users.noreply.github.com>
2024-01-11 21:04:16 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -5,3 +5,15 @@ from models.base_model import BaseModel
 from models import storage
 
 class User(BaseModel):
+	""" User class """
+	email = ""
+	password = ""
+	first_name = ""
+	last_name = ""
+	def __init__(self, *args, **kwargs):
+		""" special __init__ method """
+		if kwargs:
+			super.__init__(**kwargs)
+		else:
+			super.__init__()
+
